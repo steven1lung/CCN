0b6dab4b8a9bb8cd2b92cacff8a8f27d0d0d3d37
Steven Peko <1030steven@gmail.com>
2021-05-18 23:39:10 +0800

    project1

---
+++ project1/project1.py
@@ -0,0 +1,20 @@
+import sys
+import json
+with open("input.json","r") as f:
+    data=json.load(f)
+
+fp = open("output.txt","w")
+
+
+table=[[0 for i in range(len(data['network']))] for j in range(len(data['network']))]
+
+for i in range(len(data['network'])):
+    for j in data['network'][i][1]:
+        table[i][j]=1
+
+sys.stdout = fp
+
+for i in range(len(data['input'])):
+    for j in data['input'][i]:
+        if(table[j[0]][j[1]] == 1): print("r%d"%j[1],":",j[2])
+        else : print("r%d"%j[1],":",j,"Unreachable")
