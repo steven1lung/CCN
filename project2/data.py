f9732a57dd7a9aaf8ab05c562b7861d027983d1e
f74074106 <gtan890123@gmail.com>
2021-06-02 04:01:50 +0800

    push data

---
+++ project2/data.py
@@ -0,0 +1,14 @@
+mport json
+
+class DATA():
+    def __init__(self):
+        
+
+    def Create_data(self, route_ID, interest):
+        
+
+    def send_data(self, Infaces, route_ID, data):
+
+    
+    def On_data(self, sizes, route_ID, data, tables):
+        
