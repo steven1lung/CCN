2eeb8055e6e504a3b89431652658bf9b1a5a9cf2
f74074106 <gtan890123@gmail.com>
2021-06-02 07:37:26 +0800

    cm

---
+++ project2/ps.py
@@ -0,0 +1,19 @@
+import json
+import csv
+
+class PS():
+    def __init__(self):
+        ps_list = [] 
+
+    def Creat_ps(self, route_num, content_num, producer_content):
+
+    
+    def Get_ps(self):
+        return self.ps_list
+
+    def Search_ps_interest(self, ps, content_name):
+        if(ps.count(content_name)!= 0 )
+            return True
+        else
+            return
+
